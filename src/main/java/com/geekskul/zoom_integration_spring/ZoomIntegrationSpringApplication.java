24ab236a536eba4c46ae3192ad0258b5fd400a56
gyanendrachandrawat <gyanendrachandrawat@gmail.com>
2025-04-19 14:49:02 -0400

    first commit with hello world

---
+++ src/main/java/com/geekskul/zoom_integration_spring/ZoomIntegrationSpringApplication.java
@@ -0,0 +1,18 @@
+package com.geekskul.zoom_integration_spring;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoomIntegrationSpringApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoomIntegrationSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World....");
+	}
+}
---
+++ src/test/java/com/geekskul/zoom_integration_spring/ZoomIntegrationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekskul.zoom_integration_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoomIntegrationSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
